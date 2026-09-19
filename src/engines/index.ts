623c3b69646edf0d244ed6e87261341332273ef2
Peron <peron_ap@icloud.com>
2026-09-19 11:34:49 +0800

    chore(engines): remove DuckDuckGo

--- src/engines/index.ts
+++ src/engines/index.ts
@@ -1,12 +1,11 @@
 import { bing } from './bing';
 import { brave } from './brave';
-import { duckduckgo } from './duckduckgo';
 import { googleCse } from './google-cse';
 import type { Engine } from './common';
 
 export type { EngineResult } from './common';
 
-const engines = { bing, duckduckgo, brave, google_cse: googleCse } satisfies Record<string, Engine>;
+const engines = { bing, brave, google_cse: googleCse } satisfies Record<string, Engine>;
 export type EngineName = keyof typeof engines;
 export const engineNames = Object.keys(engines) as EngineName[];
 
